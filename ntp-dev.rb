72869afa684338f94ac63e078f97ed6c2b82aa21
barusan <info@baru-san.net>
2014-04-09 21:50:35 +0900

    update ntp-dev 4.2.7p440

--- ntp-dev.rb
+++ ntp-dev.rb
@@ -1,13 +1,24 @@
 require "formula"
 
 class NtpDev < Formula
-  url "http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-dev/ntp-dev-4.2.7p439.tar.gz"
-  version "4.2.7p439"
+  url "http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-dev/ntp-dev-4.2.7p440.tar.gz"
+  version "4.2.7p440"
   homepage "http://www.ntp.org/"
-  sha1 "35448de8085fd1f63d4c518101313f6e6a94b522"
+  sha1 "f602d4d2ce080965aaee3f1f5e4b28f633ac65a9"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  def caveats
+    s = <<-EOS.undent
+      1. make sure /usr/libexec/ntpd-wrapper calls `brew --prefix`/sbin/ntpd
+         at the last 'exec' line.
+
+      2. restart ntpd.
+          $ sudo launchctl stop org.ntp.ntpd
+    EOS
+    s
   end
 end
