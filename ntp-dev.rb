6c4c57ba19cbac37466da2e1e2549d0f7ecf57e3
barusan <info@baru-san.net>
2014-07-06 14:31:23 +0900

    update ntp-dev 4.2.7p447

--- ntp-dev.rb
+++ ntp-dev.rb
@@ -1,10 +1,10 @@
 require "formula"
 
 class NtpDev < Formula
-  url "http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-dev/ntp-dev-4.2.7p446.tar.gz"
-  version "4.2.7p446"
+  url "http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-dev/ntp-dev-4.2.7p447.tar.gz"
+  version "4.2.7p447"
   homepage "http://www.ntp.org/"
-  sha1 "634afa46e77c1fd7f0a797f4d46a4f0654b9ca8d"
+  sha1 "ffa64f2f7680cf87b388eedf2b0f5d0066abe8c6"
 
   def install
     system "./configure", "--prefix=#{prefix}"
